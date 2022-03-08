f59b8c85e16741977ed647e5283d8a628ee30c19
Appe Latte <51999459+appe-latte@users.noreply.github.com>
2022-03-08 09:40:48 -0700

    Initial Commit

---
+++ Coyne/CoyneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoyneApp.swift
+//  Coyne
+//
+//  Created by Stanford L. Khumalo on 08/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoyneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
