d5ad2bedc50f0488304f955fff19e47986883609
Appè Latte <51999459+appe-latte@users.noreply.github.com>
2022-12-06 21:35:26 -0700

    "Upgrade Account"
    - completed page
    
    --- button not active yet

--- coynitt/Coynitt/Views/AccountInfo/AccUpgradeView.swift
+++ coynitt/Coynitt/Views/AccountInfo/AccUpgradeView.swift
@@ -8,14 +8,15 @@
 import SwiftUI
 
 struct AccUpgradeView: View {
+    
     var body: some View {
+        let screenWidth = UIScreen.main.bounds.width
+        
         ZStack {
             cynWhite
             VStack {
                 
-            
-                // MARK: Card Demo
-                
+                // MARK: Card Sample
                 Rectangle()
                     .fill(LinearGradient(gradient: Gradient(colors: [cynGreen2, cynGreen]), startPoint: .topLeading, endPoint: .bottomTrailing))
                     .frame(width: 300, height: 200)
@@ -23,44 +24,181 @@ struct AccUpgradeView: View {
                     .edgesIgnoringSafeArea(.all)
                     .overlay(
                         VStack {
-                            // MARK: Top Section
+                            // MARK: Mock Card sample
                             HStack {
-                                Image("logo-trans")
+                                Image("logo")
                                     .resizable()
                                     .scaledToFill()
                                     .aspectRatio(contentMode: .fill)
                                     .clipped()
                                     .clipShape(Circle())
-                                    .frame(width: 30, height: 30)
+                                    .frame(width: 50, height: 50)
                                 
                                 Spacer()
                                 
                                 Text("COYNITT")
-                                    .font(.custom("Avenir", size: 16))
-                                    .bold()
-                                    .foregroundColor(.white)
+                                    .font(.custom("Avenir", size: 20).bold())
+                                    .foregroundColor(cynWhite)
                             }
                             .padding(.horizontal, 10)
                             
                             Spacer()
                             
-                            // MARK: Middle Section
                             VStack {
                                 HStack {
-                                    Text("Available Balance")
-                                        .font(.custom("Avenir", size: 16))
-                                        .fontWeight(.semibold)
-                                        .foregroundColor(.white)
-                                    
                                     Spacer()
+                                    
+                                    Text("YOUR NAME HERE")
+                                        .font(.custom("Avenir", size: 13))
+                                        .fontWeight(.semibold)
+                                        .foregroundColor(cynWhite)
+                                        .padding(.horizontal, 5)
+                                        .padding(.bottom, 10)
                                 }
-                               
                             }
+                            
                         }
                             .padding(10))
                 
+                Text("Your Coynitt Card")
+                    .font(.system(size: 40))
+                    .bold()
+                    .foregroundColor(.black)
+                    .minimumScaleFactor(0.5)
+                
+                Text("The digital world in your wallet")
+                    .font(.system(size: 22))
+                    .bold()
+                    .foregroundColor(cynGreen2)
+                    .minimumScaleFactor(0.5)
+                
+                VStack(alignment: .leading, spacing: 15) {
+                    
+                    // MARK: ATM withdrawal
+                    HStack {
+                        Image("withdraw")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 35, height: 35)
+                            .padding(5)
+                            .foregroundColor(.white)
+                            .background(cynRed)
+                            .clipShape(Circle())
+                        
+                        VStack(alignment: .leading) {
+                            Text("Withdraw Cash")
+                                .font(.system(size: 15))
+                                .fontWeight(.semibold)
+                                .foregroundColor(.black)
+                            
+                            Text("Use your card to withdraw cash from all ATM machines across the world that support MasterCard.")
+                                .font(.system(size: 12))
+                                .foregroundColor(.black)
+                                .multilineTextAlignment(.leading)
+                        }
+                    }
+                    .frame(width: screenWidth, height: 60)
+                    .padding(.horizontal, 20)
+                    
+                    // MARK: Shop online
+                    HStack {
+                        Image("store")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 35, height: 35)
+                            .padding(5)
+                            .foregroundColor(.white)
+                            .background(cynOrange)
+                            .clipShape(Circle())
+                            
+                        
+                        VStack(alignment: .leading) {
+                            Text("Shop Online")
+                                .font(.system(size: 15))
+                                .fontWeight(.semibold)
+                                .foregroundColor(.black)
+                            
+                            Text("Use your card to make purchases from your favourite online stores or marketplaces.")
+                                .font(.system(size: 12))
+                                .foregroundColor(.black)
+                                .multilineTextAlignment(.leading)
+                        }
+                    }
+                    .frame(width: screenWidth, height: 60)
+                    .padding(.horizontal, 20)
+                    .padding(.leading, 10)
+                    
+                    // MARK: Global Use
+                    HStack {
+                        Image("earth")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 35, height: 35)
+                            .padding(5)
+                            .foregroundColor(.white)
+                            .background(cynPurple)
+                            .clipShape(Circle())
+                        
+                        VStack(alignment: .leading) {
+                            Text("Travel Money")
+                                .font(.system(size: 15))
+                                .fontWeight(.semibold)
+                                .foregroundColor(.black)
+                            
+                            Text("Take your card with you when you travel around the world.")
+                                .font(.system(size: 12))
+                                .foregroundColor(.black)
+                                .multilineTextAlignment(.leading)
+                        }
+                    }
+                    .frame(width: screenWidth, height: 60)
+                    .padding(.horizontal, 20)
+                    
+                    // MARK: Expenses
+                    HStack {
+                        Image("graph-2")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 35, height: 35)
+                            .padding(5)
+                            .foregroundColor(.white)
+                            .background(cynGreen2)
+                            .clipShape(Circle())
+                        
+                        VStack(alignment: .leading) {
+                            Text("Manage Expenses")
+                                .font(.system(size: 15))
+                                .fontWeight(.semibold)
+                                .foregroundColor(.black)
+                            
+                            Text("Take your card with you when you travel around the world.")
+                                .font(.system(size: 12))
+                                .foregroundColor(.black)
+                                .multilineTextAlignment(.leading)
+                        }
+                    }
+                    .frame(width: screenWidth, height: 60)
+                    .padding(.horizontal, 20)
+                }
+                .frame(width: screenWidth)
+                
+                Spacer()
+                    .frame(height: 50)
+                
+                // MARK: "Upgrade" button
+                Button(action: {}, label: {
+                    Text("Upgrade Today")
+                        .fontWeight(.semibold)
+                        .frame(width: 250, height: 60)
+                        .background(cynGreen)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .foregroundColor(.white)
+                })
             }
+            .padding(.horizontal, 20)
         }
+        
+        Spacer()
     }
 }
 
